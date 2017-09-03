97189b2ca87ce8b50106bb0cd82b9d5d19d04378
K. Sabbak <ksabbak@gmail.com>
2017-09-03 11:55:09 -0500

    Add tests
    Learn about testing with Jest and Istanbul. Use them. Learn that snapshots are generated once the first time you run the test and will be the thing you compare with everafter until you run -u on the test to update the snapshot.

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -25,6 +25,7 @@ const ShowCard = props => (
 			<h3>{props.show.title}</h3>
 			<h4>({props.show.year})</h4>
 			<p>{props.show.description}</p>
+			<p>Additional thing</p>
 		</div>
 	</Wrapper>
 );
---
+++ js/__tests__/Search.spec.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+// import renderer from 'react-test-renderer';
+// Can't have renderer and enzyme in the same import
+import { shallow } from 'enzyme';
+import preload from '../../data.json';
+import Search from '../Search';
+import ShowCard from '../ShowCard';
+
+// run with -u flag to reset Snapshot (otherwise it's just the first snapshot from the first time it ran)
+// Do check-in the Snapshots to git so the tests run the same for everyone. (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧
+
+// renderer version:
+// test('Search renders correctly', () => {
+// 	const component = renderer.create(<Search />);
+// 	const tree = component.toJSON();
+// 	expect(tree).toMatchSnapshot();
+// });
+describe('Search', () => {
+	it('Search renders correctly', () => {
+		const component = shallow(<Search />);
+		expect(component).toMatchSnapshot();
+	});
+
+	it('Search should render correct amount of shows', () => {
+		const component = shallow(<Search />);
+		expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+	});
+
+	it('Search should render correct amount of shows based on search term', () => {
+		const searchWord = 'black';
+		const component = shallow(<Search />);
+		component.find('input').simulate('change', { target: { value: searchWord } });
+		const showCount = preload.shows.filter(
+			show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
+		).length;
+		expect(component.find(ShowCard).length).toEqual(showCount);
+	});
+});
